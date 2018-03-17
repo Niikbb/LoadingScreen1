df8bd2e31c907fff3775ec6b128424fed780a1aa
Niikbb <asd0l@yandex.ru>
2018-03-17 12:40:14 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -49,22 +49,7 @@ var l_background = "backgrounds/videos/glimmering.webm";
 Place one or more images
 Multiple images will automatically shuffle between them*/
 var l_bgImages = [
-	"backgrounds/images/1.jpg",
-	"backgrounds/images/2.jpg",
-	"backgrounds/images/3.jpg",
-	"backgrounds/images/4.jpg",
-	"backgrounds/images/5.jpg",
-	"backgrounds/images/6.jpg",
-	"backgrounds/images/7.jpg",
-	"backgrounds/images/8.jpg",
-	"backgrounds/images/9.jpg",
-	"backgrounds/images/10.jpg",
-	"backgrounds/images/11.jpg",
-	"backgrounds/images/12.jpg",
-	"backgrounds/images/13.jpg",
-	"backgrounds/images/14.jpg",
-	"backgrounds/images/15.jpg",
-	"backgrounds/images/16.jpg"
+	"backgrounds/images/1.jpg"
 ];
 
 /*Random background images order?*/
